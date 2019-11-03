12aa91b6988f7b9f17fb93f713af7c8982a0de0b
Francisco Miamoto <fsmiamoto@gmail.com>
2019-11-03 19:02:04 -0300

    Adds state output to UC

--- ISA_Epson_S1C17/Eq11-UC.vhd
+++ ISA_Epson_S1C17/Eq11-UC.vhd
@@ -11,34 +11,38 @@ entity uc is
         instruction : in unsigned(11 downto 0);
         clk         : in std_logic;
         rst         : in std_logic;
+        state       : out unsigned(1 downto 0);
         jump_en     : out std_logic;
         wr_en       : out std_logic
     );
 end entity;
 
 architecture a_uc of uc is
-    signal state  : unsigned(1 downto 0);
-    signal opcode : unsigned(3 downto 0);
+    signal state_s : unsigned(1 downto 0);
+    signal opcode  : unsigned(3 downto 0);
 begin
     process (clk, rst)
     begin
         if rst = '1' then
-            state <= "00";
+            state_s <= "00";
         elsif rising_edge(clk) then
-            if state = "10" then
-                state <= "00";
+            if state_s = "10" then
+                state_s <= "00";
             else
-                state <= state + 1;
+                state_s <= state_s + 1;
             end if;
         end if;
     end process;
 
+    -- Exposes internal state via state pin
+    state <= state_s;
+
     opcode <= instruction(11 downto 8);
 
-    jump_en <= '1' when state = '1' and opcode = "1111" else
+    jump_en <= '1' when state_s = "01" and opcode = "1111" else
         '0';
 
-    wr_en <= '1' when state = '1' else
+    wr_en <= '1' when state_s = "01" else
         '0';
 
 end architecture;
