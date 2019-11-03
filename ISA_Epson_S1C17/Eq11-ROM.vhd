45a19b678ee4bd0008ba35fb0379861b77a652cd
Francisco Miamoto <fsmiamoto@gmail.com>
2019-11-03 17:58:35 -0300

    Adds instructions in the ROM

--- ISA_Epson_S1C17/Eq11-ROM.vhd
+++ ISA_Epson_S1C17/Eq11-ROM.vhd
@@ -15,9 +15,17 @@ entity rom is
 end entity;
 
 architecture a_rom of rom is
-    type mem is array (0 to 183) of unsigned(11 downto 0); -- total of instructions: 184 (including variations)
+    type mem is array (0 to 183) of unsigned(15 downto 0); -- total of instructions: 184 (including variations)
     constant rom_data : mem := (
-        -- instrucoes
+    0  => b"100110_011_0000101",  -- ld %r3, 0x05 (sign-extended)
+    1  => b"100110_100_0001000",  -- ld %r4, 0x08 (sign-extended)
+    2  => b"100110_010_0000001",  -- ld %r2, 0x01 (used for subtraction)
+    3  => b"001100_101_1000_011", -- add.a %r5, %r3 ( r5 <- r5 + r3)
+    4  => b"001100_101_1000_100", -- add.a %r5, %r4 ( r5 <- r5 + r4)
+    5  => b"001100_110_1010_010", -- sub.a %r5, %r2 ( r5 <- r5 - 1)
+    6  => b"000000_110_0010100",  -- jpa 0x14
+    20 => b"001010_011_0011_101", -- ld.a %r3, %r5 (r3 <- r5)
+    21 => b"000000_110_0000011",  -- jpa 0x03
     others => (others => '0')
     );
 
