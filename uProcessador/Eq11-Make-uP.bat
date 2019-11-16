e5f8514f58083ba1bcb84c29c9632a946b816537
Joao Pedro Zanlorensi Cardoso <joaozanlorensi@gmail.com>
2019-11-16 20:56:10 -0300

    Simulação no GTKW

--- uProcessador/Eq11-Make-uP.bat
+++ uProcessador/Eq11-Make-uP.bat
@@ -7,6 +7,7 @@ C:\ghdl\ghdl\bin\ghdl.exe -a *.vhd
 C:\ghdl\ghdl\bin\ghdl.exe -e reg24bits
 C:\ghdl\ghdl\bin\ghdl.exe -e register_file
 C:\ghdl\ghdl\bin\ghdl.exe -e rom
+C:\ghdl\ghdl\bin\ghdl.exe -e flags
 C:\ghdl\ghdl\bin\ghdl.exe -e pc
 C:\ghdl\ghdl\bin\ghdl.exe -e uc
 C:\ghdl\ghdl\bin\ghdl.exe -e uprocessor
@@ -15,6 +16,6 @@ C:\ghdl\ghdl\bin\ghdl.exe -e ularegs
 C:\ghdl\ghdl\bin\ghdl.exe -e uprocessor_tb
 
 
-C:\ghdl\ghdl\bin\ghdl.exe -r uprocessor_tb --wave=Eq11-uP_tb.ghw --stop-time=6000ns
+C:\ghdl\ghdl\bin\ghdl.exe -r uprocessor_tb --wave=Eq11-uP_tb.ghw --stop-time=24000ns
 
 start cmd /c C:\ghdl\ghdl\gtkwave -f Eq11-uP_tb.ghw -a Eq11-uP_tb.gtkw
