8a687fb7a4569ded3f93b4436b5586d0990a012a
Francisco Miamoto <fsmiamoto@gmail.com>
2019-12-10 16:41:19 -0300

    Finalized RAM integration

--- uP_RAM/Eq11-ROM.vhd
+++ uP_RAM/Eq11-ROM.vhd
@@ -17,23 +17,31 @@ end entity;
 architecture a_rom of rom is
     type mem is array (0 to 127) of unsigned(15 downto 0);
     constant rom_data : mem := (
-    0  => b"100110_000_0110010",  -- ld %r0,50 
-    1  => b"100110_001_0110010",  -- ld %r1,50 
-    2  => b"100110_010_0000001",  -- ld %r2,1 
-    3  => b"100110_011_0000001",  -- ld %r3,0 
-    4  => b"001100_000_1000_001", -- add.a %r0, %r1 ( r0 <- r0 + r1) 
-    5  => b"001100_000_1000_001", -- add.a %r0, %r1 ( r0 <- r0 + r1) 
-    6  => b"001100_000_1000_001", -- add.a %r0, %r1 ( r0 <- r0 + r1) 
-    7  => b"001100_000_1000_001", -- add.a %r0, %r1 ( r0 <- r0 + r1) 
-    8  => b"100110_001_0000000",  -- ld %r1,0 
-    9  => b"001001_000_0010_001", -- ld [%r1], %r0 (stores the value in r0 to the ram address r1)
-    10 => b"001100_000_1010_010", -- sub.a %r0, %r2 ( r0 <- r0 - 1)
-    11 => b"001100_001_1000_010", -- add.a %r1, %r2 ( r1 <- r1 + 1) 
-    12 => b"100100_001_0111111",  -- cmp %r1, 63
-    13 => b"000010_000_1111100",  -- jrlt -4
-    14 => b"001100_001_1000_010", -- add.a %r3, %r2 ( r3 <- r3 + 1) 
-    15 => b"100100_001_0000010",  -- cmp %r3,2 
-    16 => b"000010_000_1111000",  -- jrlt -8
+    -- R0 will containt the value to be written to the RAM
+    0 => b"100110_000_0110010", -- ld %r0,50 
+    1 => b"100110_011_0110010", -- ld %r3,50 
+    -- Since our ld instruction uses sign extended immediate values, we load the 
+    -- inteded value divided by 4 in R3 and add it 4 times to R0
+    2 => b"001100_000_1000_011", -- add.a %r0, %r3 ( r0 <- r0 + r3) 
+    3 => b"001100_000_1000_011", -- add.a %r0, %r3 ( r0 <- r0 + r3) 
+    4 => b"001100_000_1000_011", -- add.a %r0, %r3 ( r0 <- r0 + r3) 
+    5 => b"001100_000_1000_011", -- add.a %r0, %r3 ( r0 <- r0 + r3) 
+    -- R2 loaded with 1 for increments
+    6 => b"100110_010_0000001", -- ld %r2,1 
+    -- R1 will be use for the RAM address
+    7 => b"100110_001_0000000", -- ld %r1,0 
+    -- R4 will be used for comparison with R1 and determine if the loop should continue
+    8 => b"100110_100_0100000", -- ld %r4, 32
+    -- As before, add R4 with itself to obtain 128 in it
+    9  => b"001100_100_1000_100", -- add.a %r4, %r4 ( r4 <- r4 + r4 ) 
+    10 => b"001100_100_1000_100", -- add.a %r4, %r4 ( r4 <- r4 + r4 ) 
+
+    -- Main loop
+    11 => b"001001_000_0010_001", -- ld [%r1], %r0 (stores the value in r0 to the ram address r1)
+    12 => b"001100_000_1010_010", -- sub.a %r0, %r2 ( r0 <- r0 - 1)
+    13 => b"001100_001_1000_010", -- add.a %r1, %r2 ( r1 <- r1 + 1) 
+    14 => b"001101_001_1000_100", -- cmp %r1, %r4 
+    15 => b"000010_000_1111100",  -- jrlt -4
     others => (others => '0')
     );
 
--- uP_RAM/Eq11-UC.vhd
+++ uP_RAM/Eq11-UC.vhd
@@ -50,13 +50,13 @@ begin
         end if;
     end process;
 
-    opcode             <= rom_out(15 downto 10);
-    opcode_ext         <= rom_out(6 downto 3);
+    opcode     <= rom_out(15 downto 10);
+    opcode_ext <= rom_out(6 downto 3);
 
     -- Decode registers
-    reg_write_addr     <= rom_out(9 downto 7);
-    reg_addr_1         <= rom_out(9 downto 7);
-    reg_addr_2         <= rom_out(2 downto 0);
+    reg_write_addr <= rom_out(9 downto 7);
+    reg_addr_1     <= rom_out(9 downto 7);
+    reg_addr_2     <= rom_out(2 downto 0);
 
     -- Verifies if data comes from imm or reg (1 = imm, 0 = reg)
     reg_write_data_sel <= "01" when opcode = "100110" else
@@ -72,7 +72,7 @@ begin
         '0';
 
     -- Decode immediate value
-    imm     <= rom_out(6 downto 0);
+    imm <= rom_out(6 downto 0);
 
     jump_en <= '1' when opcode = "000000" else
         '0';
@@ -86,6 +86,7 @@ begin
     ula_op_control <= "00" when opcode = "001100" and opcode_ext = "1000" else -- ADD opcode
         "01" when opcode = "001100" and opcode_ext = "1010" else                   -- SUB opcode
         "01" when opcode = "100100" else                                           -- CMP opcode
+        "01" when opcode = "001101" else                                           -- CMP 2 registers opcode
         "00";
 
     use_imm <= '1' when opcode = "100100" else
@@ -96,6 +97,7 @@ begin
         '0';
 
     flags_write_en <= '1' when opcode = "100100" and state = "10" else
+        '1' when opcode = "001101" and state = "10" else
         '0'; -- CMP opcode
 
 end architecture;
